4c96a8682a807651451867e56401e6a240230509
James Halliday <mail@substack.net>
2016-05-27 20:35:19 +0200

    appears to work

--- example/dex.js
+++ example/dex.js
@@ -9,7 +9,7 @@ var db = level('/tmp/drive.db')
 var ndb = sub(db, 'lines', { valueEncoding: 'json' })
 
 var drive = hyperdrive(sub(db, 'drive'))
-var link = process.argv[3] ? Buffer(process.argv[3], 'hex') : null
+var link = process.argv[4] ? Buffer(process.argv[4], 'hex') : null
 var archive = drive.createArchive(link, { live: true })
 if (!link) console.log(archive.key.toString('hex'))
 
@@ -25,8 +25,17 @@ var dex = hdex({
   }
 })
 
-var name = process.argv[2]
-process.stdin.pipe(archive.createFileWriteStream(name))
+var name = process.argv[3]
+if (process.argv[2] === 'add') {
+  process.stdin.pipe(archive.createFileWriteStream(name))
+} else if (process.argv[2] === 'get') {
+  dex.ready(function () {
+    ndb.get(name, function (err, lines) {
+      if (err) console.error(err)
+      else console.log(lines)
+    })
+  })
+}
 
 function countLines (stream, cb) {
   var n = 0
--- index.js
+++ index.js
@@ -1,20 +1,78 @@
 var sub = require('subleveldown')
+var inherits = require('inherits')
+var EventEmitter = require('events').EventEmitter
+var through = require('through2')
 
 var INFO = 'i', LOG = 'l'
 
 module.exports = Dex
+inherits(Dex, EventEmitter)
 
 function Dex (opts) {
-  if (!(this instanceof Dex)) return new Dex(opts)
+  var self = this
+  if (!(self instanceof Dex)) return new Dex(opts)
+  EventEmitter.call(self)
   var map = opts.map
   var archive = opts.archive
-  this.infodb = sub(opts.db, INFO)
+  var db = sub(opts.db, INFO, { valueEncoding: 'json' })
+  self._pending = 1
 
-  archive.list(function (err, entries) {
-    console.log(entries)
+  db.get('offset', function (err, offset) {
+    nonlive(offset || 0, function (err, noffset) {
+      if (err) return self.emit('error', err)
+      else live(noffset)
+    })
   })
+  function nonlive (offset, cb) {
+    archive.list({ offset: offset, live: false }, onlist)
+    function onlist (err, entries) {
+      if (err) return cb(err)
+      var files = {}
+      entries.forEach(function (e) { files[e.name] = e })
+      var keys = Object.keys(files)
+      ;(function next (err) {
+        if (err) return cb(err)
+        if (keys.length === 0) return cb(null, offset)
+        var key = keys.shift()
+        map(files[key], function (err) {
+          if (err) return cb(err)
+          db.put('offset', ++offset, next)
+        })
+      })()
+    }
+  }
+  function live (offset) {
+    process.nextTick(function () {
+      if (--self._pending === 0) self.emit('_ready')
+    })
+    var r = archive.list({ offset: offset, live: true })
+    var stream = through.obj(function (entry, enc, next) {
+      self._pending++
+      map(entry, function (err) {
+        if (err) next(err)
+        else db.put('offset', ++offset, function (err) {
+          if (err) return next(err)
+          process.nextTick(function () {
+            if (--self._pending === 0) self.emit('_ready')
+          })
+          next()
+        })
+      })
+    })
+    r.on('error', onerror)
+    stream.on('error', onerror)
+    r.pipe(stream)
+    function onerror (err) { self.emit('error', err) }
+  }
 }
 
 Dex.prototype.ready = function (fn) {
-  //...
+  var self = this
+  if (self._pending === 0) {
+    process.nextTick(function () {
+      if (self._pending === 0) {
+        fn()
+      } else self.once('_ready', fn)
+    })
+  } else self.once('_ready', fn)
 }
